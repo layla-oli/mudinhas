10c2981ff9b0d791188d54a88309fd585f5230d4
Carlos Linhares Machado <carl11br@gmail.com>
2021-09-09 15:00:02 -0300

    Parte 11: Instalando ES Lint

--- frontend/src/components/MessageBox.js
+++ frontend/src/components/MessageBox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function MessageBox(props) {
+export default function MessageBox(props){//props são os parâmetros dentro da tag 
   return (
     <div className={`alert alert-${props.variant || 'info'}`}>
       {props.children} {/*props.children é o que está entre as tags do componente MessageBox*/}
