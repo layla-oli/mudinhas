06d150daee99fbe44aa6c3998ffd216c36fce2f5
Carlos Linhares Machado <carl11br@gmail.com>
2021-09-12 18:40:30 -0300

    Adiciona detalhes a todas as plantas no data.mjs

--- backend/data.mjs
+++ backend/data.mjs
@@ -31,7 +31,7 @@ const data = {
           nome_cientifico: "Pimpinella anisum",
           preco: 15.00,
           estoque: 0,
-          detalhes: "detalhes"
+          detalhes: "A erva-doce, também conhecida como anis-verde, anis e pimpinela-branca, é uma planta medicinal da família Apiaceae que tem cerca de de 50 cm de altura, sendo composta por folhas fendidas, flores brancas e frutos secos contendo uma só semente, de sabor adocicado e aroma intenso."
         },
         {
           //id: 3,
@@ -40,7 +40,7 @@ const data = {
           nome_cientifico: "Eucalyptus deglupta",
           preco: 20.00,
           estoque: 15,
-          detalhes: "detalhes"
+          detalhes: "O eucalipto arco-íris é uma árvore alta e única, pois é o único eucalipto a viver na floresta tropical e apenas uma das quatro espécies encontradas fora da Austrália. Pode ser encontrado nas Filipinas, na Indonésia e na Papua Nova-Guiné, onde pode crescer até aos 76 metros. Embora a sua altura seja impressionante, é realmente a casca multicolorida da árvore que a destaca."
         },
         {
           //id: 4,
@@ -49,7 +49,7 @@ const data = {
           nome_cientifico: "Pereskia aculeata",
           preco: 33.15,
           estoque: 77,
-          detalhes: "detalhes"
+          detalhes: "A ora-pro-nobis, ou Pereskia aculeata, é uma cactácea folhosa, encontrada em muitas regiões no Brasil exceto o Norte, pois adapta-se melhor a climas mais secos. As mudas, no entanto, precisam de um pouco mais de umidade. Seu nome popular — ora-pro-nobis — tem a origem associada a orações de padres: Conta-se que as pessoas colhiam esta erva em terrenos de igrejas, enquanto padres entoavam orações em latim (orai por nós)."
         },
         {
           //id: 5,
@@ -58,7 +58,7 @@ const data = {
           nome_cientifico: "Aloe vera",
           preco: 40.00,
           estoque: 50,
-          detalhes: "detalhes"
+          detalhes: "A babosa é uma planta medicinal, também conhecida como Aloé vera, Caraguatá, Erva babosa, Babosa de botica ou Babosa de jardim, que pode ser utilizada em diferentes tratamentos de beleza, especialmente para melhorar a saúde do cabelo ou da pele."
         },
         {
           //id: 6,
@@ -67,7 +67,7 @@ const data = {
           nome_cientifico: "Adenium obesum",
           preco: 49.90,
           estoque: 33,
-          detalhes: "detalhes"
+          detalhes: "Adenium obesum é uma espécie de planta pertencente à família Apocynaceae, nativa das regiões do Sahel, ao sul do Saara (da Mauritânia e Senegal ao Sudão), e da África tropical e oriental e subtropical do sul e da Arábia. É popularmente chamada de rosa do deserto. Suas sementes têm sido venda fraudulenta de sementes de Adenium comum como sendo de híbridos com cores e formatos inexistentes."
         },
         {
           //id: 7,
@@ -76,7 +76,7 @@ const data = {
           nome_cientifico: "Oncidium varicosum",
           preco: 150.00,
           estoque: 15,
-          detalhes: "detalhes"
+          detalhes: "O Oncidium varicosum é uma Espécie de orquídeas do gênero Oncidium, também chamado de dama dançante e chuva-de-ouro, da subfamília Epidendroideae da familia das Orquidáceas."
         },
         {
           //id: 8,
@@ -85,7 +85,7 @@ const data = {
           nome_cientifico: "Dendrobium bigibbum",
           preco: 160.00,
           estoque: 15,
-          detalhes: "detalhes"
+          detalhes: "Dendrobium bigibbum, a flor símbolo do estado de Queensland, na Austrália, mas atualmente conhecida em quase todo território nacional. Também tem sua marca registrado devido a sua grande capacidade em florescer ao longo de quase todo ano, no entanto a tendência maior de floração dessa espécie ocorre normalmente em tempos  mais seco podendo variar entre o outono e o inverno."
         },
         {
           //id: 9,
@@ -94,7 +94,7 @@ const data = {
           nome_cientifico: "Helianthus annuus",
           preco: 32.00,
           estoque: 60,
-          detalhes: "detalhes"
+          detalhes: "O girassol (Helianthus annuus) é uma planta anual da família das Asteraceae, gênero Heliantheae. Está situado na tribo Heliantheae, subtribo Helianthinae.É cultivada pelo seu óleo e frutos comestíveis. O nome é derivado do formato de sua inflorescência."
         },
         {
           //id: 10,
@@ -103,7 +103,7 @@ const data = {
           nome_cientifico: "Impatiens parviflora",
           preco: 10.00,
           estoque: 60,
-          detalhes: "detalhes"
+          detalhes: "A Maria-sem-vergonha é uma planta da família Balsaminaceae, oriunda da África Tropical. Conhecida por diversos nomes, como beijinho, avenca, venca e beijo-turco, ela se adapta em diversos locais e é bem bonita, podendo colorir a sua casa e ajudar na decoração."
         },
         {
           //id: 11,
@@ -112,7 +112,7 @@ const data = {
           nome_cientifico: "Lavandula dentata",
           preco: 42.00,
           estoque: 12,
-          detalhes: "detalhes"
+          detalhes: "A Lavanda Francesa – Lavandula dentata é uma herbácea, pertence à família Lamiaceae, nativa do Mediterrâneo, perene, ereta, lenhosa na base, densamente ramificada, com até 1 m de altura e largura."
         },
         {
           //id: 12,
@@ -121,7 +121,7 @@ const data = {
           nome_cientifico: "Salvia rosmarinus",
           preco: 2,
           estoque: 155.00,
-          detalhes: "detalhes"
+          detalhes: "O alecrim (Salvia Rosmarinus) é uma erva aromática comum na região do Mediterrâneo ocorrendo dos 0 a 1500 metros de altitude, preferencialmente em solos de origem calcária. Devido ao seu aroma característico, os romanos designavam-no como rosmarinus, que em latim significa orvalho do mar."
         },
         {
           //id: 13,
@@ -130,7 +130,7 @@ const data = {
           nome_cientifico: "Arnica montana",
           preco: 35.00,
           estoque: 20,
-          detalhes: "detalhes"
+          detalhes: "Arnica é o nome popular da espécie vegetal de nome científico Arnica montana, uma planta originária das montanhas da Europa e da Sibéria, utilizada há muitos séculos na medicina alternativa para o tratamento da dor e inflamação de diversas condições."
         },
         {
           //id: 14,
@@ -139,7 +139,7 @@ const data = {
           nome_cientifico: "Mentha crispata",
           preco: 2.00,
           estoque: 142,
-          detalhes: "detalhes"
+          detalhes: "A hortelã comum, conhecida cientificamente como Mentha spicata, é uma planta medicinal e aromática, com propriedades que ajudam a tratar problemas digestivos, como má digestão, flatulência, enjoo ou vômitos, por exemplo, mas a hortelã também tem efeitos calmantes e expectorantes."
         },
         {
           //id: 15,
@@ -148,7 +148,7 @@ const data = {
           nome_cientifico: "Cymbopogon nardus",
           preco: 6.00,
           estoque: 93,
-          detalhes: "detalhes"
+          detalhes: "A citronela, conhecida cientificamente como Cymbopogon nardus ou Cymbopogon winterianus, é uma planta medicinal com propriedades repelentes para insetos, aromatizadora, bactericida e calmante, sendo muito utilizada na confecção de cosméticos."
         },
         {
           //id: 16,
@@ -157,7 +157,7 @@ const data = {
           nome_cientifico: "Melissa officinalis",
           preco: 3.00,
           estoque: 62,
-          detalhes: "detalhes"
+          detalhes: "Conhecida também como cidreira, melissa ou Melissa officinalis (nome científico da planta), a cidreira verdadeira é o tipo mais comum da erva. Parente da família das hortelãs, a planta varia entre 30 a 60 cm de altura e tem como origem principal algumas regiões da Europa e da Ásia. Suas folhas frescas são comumente utilizadas para o preparo de chás, sendo indicadas para auxiliar no tratamento de ansiedade, depressão e insônia."
         },
         {
           //id: 17,
@@ -166,7 +166,7 @@ const data = {
           nome_cientifico: "Stapelia hirsuta",
           preco: 22.00,
           estoque: 9,
-          detalhes: "detalhes"
+          detalhes: "O cacto estrela (Stapelia hirsuta), também conhecido por estapelia, pertence à família das Asclepiadaceae. Apesar do nome e do aspeto, esta planta não é um cacto, é uma suculenta."
         },
         {
           //id: 18,
@@ -175,16 +175,16 @@ const data = {
           nome_cientifico: "Syzygium aromaticum",
           preco: 20.00,
           estoque: 30,
-          detalhes: "detalhes"
+          detalhes: " cravinho ou cravo-da-índia, chamado cientificamente Syzygium aromaticus, tem ação medicinal sendo útil no combate a dores, infecções, além de ter propriedades medicinais também é uma importante fonte de nutrientes, como vitamina A, vitamina E e beta-caroteno. "
         },
         {
           //id: 19,
           imagem:'/images/products/Saintpaulia_ionantha.jpg',
-          nome_popular: "Violeta Lilás (muda)",
+          nome_popular: "Violeta Africana (muda)",
           nome_cientifico: "Saintpaulia ionantha",
           preco: 5.00,
           estoque: 44,
-          detalhes: "detalhes"
+          detalhes: "A Violeta Africana – Saintpaulia ionantha é uma herbácea, pertence à família Gesneriaceae, nativa da África Tropical, perene, acaule, de 15-20 cm de altura e raízes curtas."
         },
         {
           //id: 20,
@@ -193,7 +193,7 @@ const data = {
           nome_cientifico: "Epipremnum aureum",
           preco: 27.00,
           estoque: 16,
-          detalhes: "detalhes"
+          detalhes: "Epipremnum aureum, conhecida pelos nomes comuns de jibóia ou hera-do-diabo, é uma planta da família das Araceae, originária das Ilhas Salomão. A espécie é uma trepadeira sarmentosa, crescendo até 10 m de comprimento, com flores insignificantes e propagação por estaquia de ramos."
         }
       ]
     }
